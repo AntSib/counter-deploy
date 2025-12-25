4f0f8a8fcf9bdd9d121ef637a5252d524019f55a
AntSib <antonsibilev2005@gmail.com>
2025-12-25 15:04:34 +0300

    Small fix

--- backend/tests/test_counter.py
+++ backend/tests/test_counter.py
@@ -1,10 +1,12 @@
 def test_initial_value_is_zero(client):
+    """Check that the initial value of the counter is zero."""
     response = client.get("/api/counter")
     assert response.status_code == 200
     assert response.json["value"] == 0
 
 
 def test_increment_increases_value(client):
+    """Check that the counter increments by one each time we call /api/counter/increment."""
     response = client.post("/api/counter/increment")
     assert response.status_code == 200
     assert response.json["value"] == 1
@@ -14,6 +16,7 @@ def test_increment_increases_value(client):
 
 
 def test_counter_cannot_be_negative(client):
+    """Check that the counter cannot be decremented below zero."""
     for _ in range(5):
         response = client.post("/api/counter/decrement")
         assert response.status_code == 400
@@ -23,6 +26,7 @@ def test_counter_cannot_be_negative(client):
 
 
 def test_double_increment(client):
+    """Check that the counter increments by one each time we call /api/counter/increment twice."""
     response = client.post("/api/counter/increment")
     assert response.json["value"] == 1
 
@@ -31,5 +35,6 @@ def test_double_increment(client):
 
 
 def test_any_route(client):
+    """Check that any route not defined in the API returns a 404."""
     response = client.get("/api/counter/blah-blah-blah")
     assert response.status_code == 404
